53ebe37a2f19d284feed2508ecb67c0a39af909b
James C <you@example.com>
2022-07-13 20:52:35 -0700

    refactor

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -2,33 +2,47 @@ import styled from 'styled-components';
 
 const CardWrapper = styled.div`
     display: flex;
+    jusify-content: space-evenly;
     flex: 1;
-    border: 1px solid red:
+    border: 1px solid red;
     margin-top: 2vh;
+    padding: 2vh 1vw 2vh 1vw;
+    max-height: 10vh;
     `;
 
     const ContactDetails = styled.div`
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    border: 1px solid black;
+    `;
+
+    const CloseButton = styled.div`
+    display: flex;
+    flex: 1;
+    align-self: center;
+    justify-content: flex-end;
+    `;
+
+    const ProfilePic = styled.img`
+    border-radius: 50%;
+    max-height: 100%;
     `;
 
 const ContactCard = ({ firstName='', lastName='', phoneNumber='', profilePic='' }) => {
     return (
-        <CardWrapper>
-        <div>
+        <CardWrapper onClick={() => console.log("Hello, world.")}>
             <ContactDetails>
                 <div>
-                    <img src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
+                    <ProfilePic src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
                 </div>
                 <div>
                     <p>{firstName} {lastName}</p>
                     <p>{phoneNumber}</p>
                 </div>
             </ContactDetails>
-            <div>
-                <button>X</button>
-            </div>
-        </div>
+            <CloseButton>
+                <button onClick={() => null}>X</button>
+            </CloseButton>
         </CardWrapper>
     )
 }
